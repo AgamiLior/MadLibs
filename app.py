db3579bbc3c993af9ad641442865b1a49e7c8c43
Paul Sherer <wolfymaster@hotmail.com>
2021-07-09 16:30:14 +0000

    use stories class

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from stories import Story
 
 
 app = Flask(__name__)
@@ -20,5 +21,36 @@ def form():
     verb = request.args["verb"]
     adjective = request.args["adjective"]
     plural_noun = request.args["plural_noun"]
-    return render_template("form.html",place=place, noun = noun, verb = verb, adjective = adjective, plural_noun = plural_noun, option = option)
+    
+    story1 = Story(
+        ["place", "noun", "verb", "adjective", "plural_noun"],
+        """Once upon a time in a long-ago {place}, there lived a
+        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+    )
+
+    story2 = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """I just love to go to {place}, there is a lot of {adjective} {noun}.
+    I am interested in {verb} {plural_noun}."""
+    )
+
+    if option == 'Story1':
+        text = story1.generate({
+            'place': place, 
+            'noun': noun, 
+            'verb': verb, 
+            'adjective': adjective, 
+            'plural_noun': plural_noun
+        })
+    else:
+        text = story2.generate({
+            'place': place, 
+            'noun': noun, 
+            'verb': verb, 
+            'adjective': adjective, 
+            'plural_noun': plural_noun
+        })
+    
+
+    return render_template("form.html", story = text)
 
